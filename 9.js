60e13450080e9319d09dfcfcfc32c4ba4818ed19
dai998812 <118254502+dai998812@users.noreply.github.com>
2022-11-15 15:54:53 +0700

    upload

---
+++ 9.js
@@ -0,0 +1,29 @@
+function nhaptentuoi()
+{
+    var ten,tuoi;
+    ten=prompt("ban hay nhap ten");
+    if(ten==null)
+    {
+        document.write("ban da khong nhap ten")
+    }
+    else{
+        document.write("ten cua ban la:<u>"+ ten+"</u><br>")
+    }
+
+    tuoi=prompt("ban hay nhap tuoi");
+    if(tuoi==null)
+    {
+        document.write("ban da khong nhap tuoi")
+    }
+    else{
+        document.write("tuoi cua ban la:<b>"+ tuoi+"</b>")
+    }
+  
+  
+   
+  
+    
+  
+    
+  
+ }
